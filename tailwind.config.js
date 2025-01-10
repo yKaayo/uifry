6a47c5afe330508d8961734c7ebaeba44b942578
yKaayo <caiokaea@gmail.com>
2025-01-09 21:33:48 -0300

    Footer Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        'light-gray': '#57585f'
+      },
       fontSize: {
         'clamp-6xl': 'clamp(36px, 4vw, 60px)'
       },
