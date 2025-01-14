bf2c41d2a0f11bfea403a4327acd9c5807c327cc
yKaayo <caiokaea@gmail.com>
2025-01-14 11:48:22 -0300

    Testimonial Done

--- src/main.js
+++ src/main.js
@@ -1,3 +1,58 @@
+// GSAP
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+if (window.innerWidth <= 768) {
+  gsap.utils.toArray(".top-to-bottom").forEach((box) => {
+    gsap.to(box, {
+      y: -200,
+      opacity: 1,
+      scale: 1,
+      scrollTrigger: {
+        trigger: box,
+        start: "top 80%",
+        end: "50% center",
+        scrub: true,
+        markers: false,
+      },
+    });
+  });
+}
+
+if (window.innerWidth > 768) {
+  gsap.utils.toArray(".right-to-left").forEach((box) => {
+    gsap.to(box, {
+      x: -200,
+      opacity: 1,
+      scale: 1,
+      scrollTrigger: {
+        trigger: box,
+        start: "top 90%",
+        end: "50% 50%",
+        scrub: true,
+        markers: false,
+      },
+    });
+  });
+
+  gsap.utils.toArray(".left-to-right").forEach((box) => {
+    gsap.to(box, {
+      x: 200,
+      opacity: 1,
+      scale: 1,
+      scrollTrigger: {
+        trigger: ".left-to-right",
+        start: "top 90%",
+        end: "50% 50%",
+        scrub: true,
+        markers: false,
+      },
+    });
+  });
+}
+
 // Theme Mode
 document.documentElement.classList.toggle(
   "dark",
@@ -26,59 +81,29 @@ menuButton.addEventListener("click", () => {
   menuButton.querySelector(".bi").classList.toggle("bi-x-lg");
 });
 
-// GSAP
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+// Testimonial
+const people = document.querySelectorAll(".testimonial__people");
 
-gsap.registerPlugin(ScrollTrigger);
+const names = [
+  "Nick Jonas",
+  "Camila Ferreira",
+  "Lucas Monteiro",
+  "Sofia Oliveira",
+  "Isabela Rocha",
+];
 
-if (window.innerWidth <= 768) {
-  gsap.utils.toArray(".top-to-bottom").forEach((box) => {
-    gsap.to(box, {
-      y: -200,
-      opacity: 1,
-      scale: 1,
-      scrollTrigger: {
-        trigger: box,
-        start: "top 80%",
-        end: "50% 50%",
-        scrub: true,
-        markers: false,
-      },
-    });
+people.forEach((person) => {
+  person.addEventListener("click", () => {
+    document
+      .querySelectorAll(".testimonial__people")
+      .forEach((item) => item.classList.remove("active"));
+
+    person.classList.add("active");
+
+    for (let i = 0; i < people.length; i++) {
+      person == people[i]
+        ? (document.querySelector("#namePerson").textContent = names[i])
+        : null;
+    }
   });
-}
-
-if (window.innerWidth > 768) {
-  gsap.utils.toArray(".right-to-left").forEach((box) => {
-    gsap.to(box, {
-      x: -200,
-      opacity: 1,
-      scale: 1,
-      scrollTrigger: {
-        trigger: box,
-        start: "top 90%",
-        end: "50% 50%",
-        scrub: true,
-        markers: false,
-      },
-    });
-  });
-}
-
-if (window.innerWidth > 768) {
-  gsap.utils.toArray(".left-to-right").forEach((box) => {
-    gsap.to(box, {
-      x: 200,
-      opacity: 1,
-      scale: 1,
-      scrollTrigger: {
-        trigger: ".left-to-right",
-        start: "top 90%",
-        end: "50% 50%",
-        scrub: true,
-        markers: false,
-      },
-    });
-  });
-}
+});
